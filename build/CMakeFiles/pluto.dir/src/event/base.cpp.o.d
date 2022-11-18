71c28a4ecc5efde8cbb33db30c8bde8cb7b79d11
Ethan <91308860+ethan706@users.noreply.github.com>
2022-11-18 22:00:10 +0800

    Delete build directory

--- build/CMakeFiles/pluto.dir/src/event/base.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/pluto.dir/src/event/base.cpp.o: \
- /run/media/ethan706/Code/pluto/src/event/base.cpp \
- /usr/include/stdc-predef.h \
- /run/media/ethan706/Code/pluto/include/pluto/event/base.h
